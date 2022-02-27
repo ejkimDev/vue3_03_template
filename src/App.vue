fa9a71b8098e158246a6828f066fcbb1001732fc
ejkimDev <ejkim.dev@gmail.com>
2022-02-28 03:58:06 +0900

    vue3 lifecycle

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 2
+    }
+  },
+  /*
+  * 라이프사이클 (작성 순서는 상관X)
+  * beforeCreate : 컴포넌트가 생성되기 직전에 실행, 활용도가 적다.
+  * created : App.vue라는 컴포넌트를 초기화하는 과정에서 컴포넌트가 생성된 직후에 실행
+  * beforeMount : 컴포넌트가 HTML 구조에 연결되기 전
+  * mounted : 컴포넌트가 HTML 구조에 연결된 후. 화면에 컴포넌트가 출력된 후 실행
+   */
+  beforeCreate() {
+    console.log('beforeCreate', this.count);
+  },
+  created() {   
+    console.log('Created', this.count);
+  },
+  beforeMount() {
+    console.log('beforeMount');
+    console.log(document.querySelector('h1'));
+  },
+  mounted() {
+    console.log('Mounted');
+    console.log(document.querySelector('h1'));
+  }
+}
+</script>
---
+++ src/components/Fruit.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>{{ name }}?!</li>
+</template>
+
+<script>
+export default {   
+  props: {
+    name: {     // type이 String인 name이라는 데이터를 받는다.(데이터가 들어오지 않으면 default로 ''를 사용하겠다.)
+      type: String,
+      default:  ''
+    }
+  }
+
+}
+</script>
+
+<style scoped lang="scss">
+/* scoped : style태그는 다른 component는 영향을 미치지 않음. -> 유효범위 : 현재의 component */
+  h1 {
+    color:  red !important;
+  }
+</style>
