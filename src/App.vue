55958fd3327292d6070f9bff711e3170c185d2db
ejkimDev <ejkim.dev@gmail.com>
2022-03-01 02:46:47 +0900

    app.vue update

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,30 @@
 <template>
-  <h1>{{ count }}</h1>
+  <h1 
+    :[attr]="'active'"
+    @[event]="add"
+    >{{ msg }}</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      count: 2
+      msg : 'active',
+      attr : 'class',
+      event : 'click'
     }
   },
-  /*
-  * 라이프사이클 (작성 순서는 상관X)
-  * beforeCreate : 컴포넌트가 생성되기 직전에 실행, 활용도가 적다.
-  * created : App.vue라는 컴포넌트를 초기화하는 과정에서 컴포넌트가 생성된 직후에 실행
-  * beforeMount : 컴포넌트가 HTML 구조에 연결되기 전
-  * mounted : 컴포넌트가 HTML 구조에 연결된 후. 화면에 컴포넌트가 출력된 후 실행
-   */
-  beforeCreate() {
-    console.log('beforeCreate', this.count);
-  },
-  created() {   
-    console.log('Created', this.count);
-  },
-  beforeMount() {
-    console.log('beforeMount');
-    console.log(document.querySelector('h1'));
-  },
-  mounted() {
-    console.log('Mounted');
-    console.log(document.querySelector('h1'));
+  methods: {
+    add() {
+      this.msg += '!';
+    }
   }
 }
 </script>
+
+<style>
+  .active { 
+    color: royalblue;
+    font-size: 40px;
+  }
+</style>
